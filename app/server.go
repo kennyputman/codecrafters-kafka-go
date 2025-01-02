3b8e5af52ce7015fd1752db7171b87a90c7ae187
Kenny Putman <kbputman@gmail.com>
2025-01-02 12:02:55 -0600

    add response handler

--- app/server.go
+++ app/server.go
@@ -7,14 +7,45 @@ import (
 	"os"
 )
 
-func responeResolver() []byte {
-	msgSize := int32(4)
-	correlationId := int32(7)
+type Message struct {
+	Size   uint32
+	Header Header
+}
+
+type Header struct {
+	RequestApiKey     uint16
+	RequestApiVersion uint16
+	CorrelationId     uint32
+}
+
+func parseRequest(msg []byte) (Message, error) {
+	msgSize := binary.BigEndian.Uint32(msg[:4])
+	reqApiKey := binary.BigEndian.Uint16(msg[4:6])
+	reqApiVersion := binary.BigEndian.Uint16(msg[6:8])
+	correlationId := binary.BigEndian.Uint32(msg[8:12])
+
+	res := Message{
+		Size: msgSize,
+		Header: Header{
+			RequestApiKey:     reqApiKey,
+			RequestApiVersion: reqApiVersion,
+			CorrelationId:     correlationId,
+		},
+	}
+	return res, nil
+}
+
+func responeResolver(msg []byte) []byte {
+
+	req, err := parseRequest(msg)
+	if err != nil {
+		fmt.Printf("Error, %e", err)
+	}
 
 	response := make([]byte, 8)
 
-	binary.BigEndian.PutUint32(response[0:4], uint32(msgSize))
-	binary.BigEndian.PutUint32(response[4:8], uint32(correlationId))
+	binary.BigEndian.PutUint32(response[0:4], req.Size)
+	binary.BigEndian.PutUint32(response[4:8], req.Header.CorrelationId)
 
 	return response
 }
@@ -23,12 +54,15 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
 		buff := make([]byte, 4096)
-		_, err := conn.Read(buff)
+		n, err := conn.Read(buff)
 		if err != nil {
 			os.Exit(1)
 		}
 
-		resp := responeResolver()
+		msg := make([]byte, n)
+		msg = buff[:n]
+
+		resp := responeResolver(msg)
 		conn.Write(resp)
 	}
 }
